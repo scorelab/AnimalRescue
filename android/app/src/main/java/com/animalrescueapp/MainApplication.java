74767cb9cdd18c1fd593b147f379a83bdfcd58f9
wathsara daluwatta <wvd.51461@gmail.com>
2019-05-03 18:15:30 +0530

    Initialized the Map

--- android/app/src/main/java/com/animalrescueapp/MainApplication.java
+++ android/app/src/main/java/com/animalrescueapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.animalrescueapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new MapsPackage(),
             new ImagePickerPackage(),
             new RNGestureHandlerPackage(),
             new VectorIconsPackage()
