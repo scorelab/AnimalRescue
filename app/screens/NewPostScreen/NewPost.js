33bba3ff799fc8f84f6104d337cd5f20b13a731c
wathsara daluwatta <wvd.51461@gmail.com>
2019-05-03 16:17:29 +0530

    Created the Image Picking

--- app/screens/NewPostScreen/NewPost.js
+++ app/screens/NewPostScreen/NewPost.js
@@ -1,20 +1,65 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View,Image, TouchableOpacity, PermissionsAndroid } from 'react-native';
 import Header from "../../components/HeaderNavigationBar/HeaderNavigationBar";
 import { ProgressSteps, ProgressStep } from 'react-native-progress-steps';
-import { COLOR_PRIMARY , COLOR_BLACK } from "../../config/styles" ;
+import ImagePicker from "react-native-image-picker";
+import { COLOR_PRIMARY, COLOR_BLACK } from "../../config/styles";
 import styles from "./style";
 class NewPost extends React.Component {
 
     constructor() {
         super()
+        this.state = {
+            photoError: true,
+            Location: false,
+            Information: false,
+            pickedImage: null
+        }
 
     }
 
-    componentDidMount() {
-
+    componentDidMount = () => {
+        this.requestCameraPermission();
+    }
+    requestCameraPermission = async () => {
+        try {
+            const granted = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.CAMERA,
+                {
+                    title: 'Camera Permission',
+                    message:
+                        'Animal Rescue App needs access to your camera ' +
+                        'so you can take awesome pictures.',
+                    buttonNeutral: 'Ask Me Later',
+                    buttonNegative: 'Cancel',
+                    buttonPositive: 'OK',
+                },
+            );
+            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                console.log('You can use the camera');
+            } else {
+                console.log('Camera permission denied');
+            }
+        } catch (err) {
+            console.warn(err);
+        }
     }
 
+    selectPhoto = () => {
+        ImagePicker.showImagePicker({ title: "Pick an Image", maxWidth: 800, maxHeight: 600 }, res => {
+            if (res.didCancel) {
+                console.log("User cancelled!");
+            } else if (res.error) {
+                console.log("Error", res.error);
+            } else {
+                this.setState({
+                    pickedImage: res.uri,
+                    photoError: false
+                });
+            }
+        });
+
+    };
 
     render() {
 
@@ -24,9 +69,21 @@ class NewPost extends React.Component {
                 <Header title="New Post" />
                 <View style={styles.container}>
                     <ProgressSteps activeStepIconBorderColor={COLOR_PRIMARY} completedProgressBarColor={COLOR_PRIMARY} completedStepIconColor={COLOR_PRIMARY} activeLabelColor={COLOR_PRIMARY} labelColor={COLOR_BLACK}>
-                        <ProgressStep label="Photo" previousBtnDisabled={true} nextBtnStyle={styles.nextBtn} nextBtnTextStyle={styles.nextBtnText}>
+                        <ProgressStep label="Photo" errors={this.state.photoError} previousBtnDisabled={true} nextBtnStyle={styles.nextBtn} nextBtnTextStyle={styles.nextBtnText}>
                             <View style={styles.stepContainer}>
-                                <Text>This is the content within step 1!</Text>
+                                {this.state.photoError == true ? (
+                                    <TouchableOpacity style={styles.imageContainer} onPress={() => this.selectPhoto()}>
+                                        {/* <View style={styles.imageContainer}> */}
+                                        <Text>Select an Image</Text>
+                                        {/* </View> */}
+                                    </TouchableOpacity>
+                                ) : (
+                                        <TouchableOpacity style={styles.imageContainer} onPress={() => this.selectPhoto()}>
+                                            <Image source={{ uri: this.state.pickedImage }} style={{width:'100%',height:'100%'}}/>
+                                        </TouchableOpacity>
+
+                                    )}
+
                             </View>
                         </ProgressStep>
                         <ProgressStep label="Location" previousBtnStyle={styles.nextBtn} previousBtnTextStyle={styles.preBtnText} nextBtnStyle={styles.nextBtn} nextBtnTextStyle={styles.nextBtnText}>
--- app/screens/NewPostScreen/style.js
+++ app/screens/NewPostScreen/style.js
@@ -1,60 +1,68 @@
 import { StyleSheet, Dimensions } from "react-native";
-import { COLOR_PRIMARY } from "../../config/styles" ;
+import { COLOR_PRIMARY, COLOR_GRAY } from "../../config/styles";
 
 let deviceWidth = Dimensions.get("window").width;
 let deviceHeight = Dimensions.get("window").height;
 
 export default StyleSheet.create({
-container: { 
-    flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center'
- },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
 
- stepContainer:{
-     alignItems: 'center'
- },
- nextBtn: {
-    backgroundColor: '#192f6a',
-    borderRadius: 13,
-    width: 70
-},
-preBtn: {
-    backgroundColor: '#192f6a',
-    borderRadius: 13,
-    width: 70
-},
-nextBtnText: {
-    color: 'white',
-    fontSize: 16,
-    textAlign: 'center'
-},
-preBtnText: {
-    color: 'white',
-    fontSize: 16,
-    textAlign: 'center'
-},
-leftIconContainer: {
-    marginLeft: 10,
-    marginTop: 5,
-    borderRadius: 5
-},
-rightIconContainer: {
-    marginLeft: 10,
-    marginTop: 5,
-    borderRadius: 5
-},
-titleArea:{
-    width:deviceWidth*0.5,
-    alignItems:"center"
-},
+    stepContainer: {
+        flex: 1,
+        height:'100%',
+        alignItems: 'center'        
+    },
+    nextBtn: {
+        backgroundColor: '#192f6a',
+        borderRadius: 13,
+        width: 70
+    },
+    preBtn: {
+        backgroundColor: '#192f6a',
+        borderRadius: 13,
+        width: 70
+    },
+    nextBtnText: {
+        color: 'white',
+        fontSize: 16,
+        textAlign: 'center'
+    },
+    preBtnText: {
+        color: 'white',
+        fontSize: 16,
+        textAlign: 'center'
+    },
+    imageContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: COLOR_GRAY,
+        borderWidth: 0.8,
+        borderStyle: 'dashed',
+        width: '90%',
+        minHeight: deviceHeight*0.4
 
-//Text
+    },
+    rightIconContainer: {
+        marginLeft: 10,
+        marginTop: 5,
+        borderRadius: 5
+    },
+    titleArea: {
+        width: deviceWidth * 0.5,
+        alignItems: "center"
+    },
 
-titleFont:{
-    fontSize:18,
-    color:"white"
-}
+    //Text
+
+    titleFont: {
+        fontSize: 18,
+        color: "white"
+    }
 
 
 
