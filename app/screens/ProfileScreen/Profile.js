aaa1a4383c27e9672d6f27d8dcc209a91cb483fd
wathsara daluwatta <wvd.51461@gmail.com>
2019-05-06 15:21:19 +0530

    Creted the Image Picker

--- app/screens/ProfileScreen/Profile.js
+++ app/screens/ProfileScreen/Profile.js
@@ -3,16 +3,70 @@ import { Text, View, TouchableOpacity, Image, ScrollView, Linking } from 'react-
 import Header from "../../components/HeaderNavigationBar/HeaderNavigationBar";
 import styles from "./style";
 import Ionicons from "react-native-vector-icons/FontAwesome";
-import { COLOR_PRIMARY, COLOR_SECONDARY, COLOR_LIGHT, COLOR_GRAY } from "../../config/styles";
+import ImagePicker from "react-native-image-picker";
 class Profile extends React.Component {
 
     constructor() {
         super()
+        this.state = {
+            newProfileImage:null,
+            newCoverPhoto:null
+        }
 
     }
-
+    requestCameraPermission = async () => {
+        try {
+            const granted = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.CAMERA,
+                {
+                    title: 'Camera Permission',
+                    message:
+                        'Animal Rescue App needs access to your camera ' +
+                        'so you can take awesome pictures.',
+                    buttonNeutral: 'Ask Me Later',
+                    buttonNegative: 'Cancel',
+                    buttonPositive: 'OK',
+                },
+            );
+            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                console.log('You can use the camera');
+            } else {
+                console.log('Camera permission denied');
+            }
+        } catch (err) {
+            console.warn(err);
+        }
+    }
     componentDidMount() {
+        this.requestCameraPermission();
+    }
 
+    editProfilePicture = () => {
+        ImagePicker.showImagePicker({ title: "Pick a Profile Picture", maxWidth: 800, maxHeight: 600 }, res => {
+            if (res.didCancel) {
+                console.log("User cancelled!");
+            } else if (res.error) {
+                console.log("Error", res.error);
+            } else {
+                this.setState({
+                    newProfileImage: res.uri,                    
+                });
+            }
+        });
+    }
+
+    editCoverPicture = () => {
+        ImagePicker.showImagePicker({ title: "Pick a Cover Photo", maxWidth: 800, maxHeight: 600 }, res => {
+            if (res.didCancel) {
+                console.log("User cancelled!");
+            } else if (res.error) {
+                console.log("Error", res.error);
+            } else {
+                this.setState({
+                    newCoverPhoto: res.uri,                    
+                });
+            }
+        });
     }
 
     openMap = () => {
@@ -35,13 +89,13 @@ class Profile extends React.Component {
                 <ScrollView>
                     <View style={styles.header}>
                         <Image style={{ width: '100%', height: '100%' }} source={require("../../images/dog.jpg")} />
-                        <TouchableOpacity style={styles.editCover}>
+                        <TouchableOpacity style={styles.editCover} onPress={()=> this.editCoverPicture()}>
                             <Ionicons name={'camera'} size={20} color={'#000'} />
                             <Text> EDIT</Text>
                         </TouchableOpacity>
                     </View>
                     <Image style={styles.avatar} source={require("../../images/user_image_1.jpg")} />
-                    <TouchableOpacity style={styles.editProfilePic}>
+                    <TouchableOpacity style={styles.editProfilePic} onPress={()=> this.editProfilePicture()}>
                         <Ionicons name={'camera'} size={20} color={'#000'} />
                     </TouchableOpacity>
                     <View style={styles.body}>
