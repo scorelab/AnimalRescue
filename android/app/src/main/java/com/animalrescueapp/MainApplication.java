de95a3cd8c3a3dcdddcbdd716510f6721b40bc31
wathsara daluwatta <wvd.51461@gmail.com>
2019-05-08 09:02:07 +0530

    Initialized the fbsdk

--- android/app/src/main/java/com/animalrescueapp/MainApplication.java
+++ android/app/src/main/java/com/animalrescueapp/MainApplication.java
@@ -3,6 +3,9 @@ package com.animalrescueapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
 import com.swmansion.reanimated.ReanimatedPackage;
 import com.azendoo.reactnativesnackbar.SnackbarPackage;
 import com.airbnb.android.react.maps.MapsPackage;
@@ -13,12 +16,16 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
 import java.util.Arrays;
 import java.util.List;
-
+import com.facebook.appevents.AppEventsLogger;
+import android.content.Intent;
 public class MainApplication extends Application implements ReactApplication {
-
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+ 
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
@@ -26,9 +33,10 @@ public class MainApplication extends Application implements ReactApplication {
     }
 
     @Override
-    protected List<ReactPackage> getPackages() {
+    protected List<ReactPackage> getPackages() {      
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FBSDKPackage(mCallbackManager),
             new ReanimatedPackage(),
             new SnackbarPackage(),
             new MapsPackage(),
@@ -51,7 +59,8 @@ public class MainApplication extends Application implements ReactApplication {
 
   @Override
   public void onCreate() {
-    super.onCreate();
+    super.onCreate();       
     SoLoader.init(this, /* native exopackage */ false);
   }
+  
 }
