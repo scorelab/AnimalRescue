b3316aa3595679e54814bba237f890db8eacad72
wathsara daluwatta <wvd.51461@gmail.com>
2019-05-11 23:04:38 +0530

    updated the App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { createStackNavigator, createBottomTabNavigator, createAppContainer } from "react-navigation";
-import { StyleSheet, Text, View, Container } from 'react-native';
+import { StyleSheet, TouchableOpacity, View, Container } from 'react-native';
 import Ionicons from "react-native-vector-icons/FontAwesome";
 import Home from "./app/screens/HomeScreen/Home";
 import Auth from "./app/screens/AuthScreen/Auth";
@@ -11,94 +11,104 @@ import Notification from "./app/screens/NotificationScreen/notificationScreen";
 import Post from "./app/screens/PostScreen/Post";
 import Comment from "./app/screens/CommentScreen/Comment";
 import { Badge } from 'react-native-elements';
+
 console.disableYellowBox = true;
-const AppStack = createBottomTabNavigator(
-  {
-    Home: { screen: Home },
-    New: { screen: NewPost },
-    Profile: { screen: Profile },
-    Notification: {
-      screen: Notification,
-      navigationOptions: {
-        tabBarLabel: "Notifications",
-        tabBarIcon: ({ tintColor }) => (
-          <View>
-            {1 != 0 ? (
-              <View>
-                <Ionicons name="bell" size={25} color={tintColor} />
-                <Badge status="error" value="10" containerStyle={{ position: 'absolute', top: -4, right: -4 }} />
-              </View>
-            ) : (
-                <View>
-                  <Ionicons name="bell" size={25} color={tintColor} />
-                </View>
-              )}
-          </View>
-
-        )
-      },
-    }
-  },
-  {
-    defaultNavigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({ focused, tintColor }) => {
-
-        const { routeName } = navigation.state;
-        let iconName;
-        if (routeName === "Home") {
-          iconName = "home";
-        } else if (routeName === "New") {
-          iconName = "plus-square";
-        } else if (routeName === "Profile") {
-          iconName = "user";
-        }
-        return <Ionicons name={iconName} size={25} color={tintColor} />;
-      }
-    }),
-    tabBarOptions: {
-      activeTintColor: "#4885ed",
-      inactiveTintColor: "white",
-      style: {
-        backgroundColor: '#192f6a',
-      }
-    },
-  }
-);
-
-const Stack = createStackNavigator(
-  {
-    Auth: {
-      screen: Auth
-    },
-    Post: {
-      screen: Post
-    },
-    Comment: {
-      screen: Comment
-    },
-    App: {
-      screen: AppStack,
-      navigationOptions: {
-        gesturesEnabled: true,
-      },
-    }
-  }, {
-    initialRouteName: "Auth",
-    headerMode: "none"
-  }
-)
-
-const AppContainer = createAppContainer(Stack);
 
 export default class App extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      count:10
+      count:2
     }
   }  
+
+  click = () =>{
+    this.setState({
+      count:0
+    })
+  }
+  componentDidMount(){
+    
+  }
   render() {
+    const AppStack = createBottomTabNavigator(
+      {
+        Home: { screen: Home },
+        New: { screen: NewPost },
+        Profile: { screen: Profile },
+        Notification: {
+          screen: Notification,
+          navigationOptions: {
+            tabBarLabel: "Notifications",                        
+            tabBarIcon: ({ tintColor }) => (
+              <View>
+                {this.state.count > 0 ? (
+                  <View>
+                    <Ionicons name="bell" size={25} color={tintColor} />
+                    <Badge status="error" value={this.state.count} containerStyle={{ position: 'absolute', top: -4, right: -4 }} />
+                  </View>
+                ) : (
+                    <View>
+                      <Ionicons name="bell" size={25} color={tintColor} />
+                    </View>
+                  )}
+              </View>
+    
+            )
+          },
+        }
+      },
+      {
+        defaultNavigationOptions: ({ navigation }) => ({
+          tabBarIcon: ({ focused, tintColor }) => {
+    
+            const { routeName } = navigation.state;
+            let iconName;
+            if (routeName === "Home") {
+              iconName = "home";              
+            } else if (routeName === "New") {
+              iconName = "plus-square";
+            } else if (routeName === "Profile") {
+              iconName = "user";
+            }
+            return <Ionicons name={iconName} size={25} color={tintColor} />;
+          }
+        }),
+        tabBarOptions: {
+          activeTintColor: "#4885ed",
+          inactiveTintColor: "white",
+          style: {
+            backgroundColor: '#192f6a',
+          }
+        },
+      }
+    );
+    
+    const Stack = createStackNavigator(
+      {
+        Auth: {
+          screen: Auth
+        },
+        Post: {
+          screen: Post
+        },
+        Comment: {
+          screen: Comment
+        },
+        App: {
+          screen: AppStack,
+          navigationOptions: {
+            gesturesEnabled: true,
+          },
+        }
+      }, {
+        initialRouteName: "Auth",
+        headerMode: "none"
+      }
+    )
+    
+    const AppContainer = createAppContainer(Stack);
+    
     return (
 
       <AppContainer/>
