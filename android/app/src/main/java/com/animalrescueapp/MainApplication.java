0c4cbd0e9a3ef70dbd5ddced2e2325038dd4840c
wathsara daluwatta <wvd.51461@gmail.com>
2019-05-14 12:47:43 +0530

    Added the Bottom sheet

--- android/app/src/main/java/com/animalrescueapp/MainApplication.java
+++ android/app/src/main/java/com/animalrescueapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.animalrescueapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import ui.bottomactionsheet.RNBottomActionSheetPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookSdk;
@@ -36,6 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {      
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNBottomActionSheetPackage(),
             new FBSDKPackage(mCallbackManager),
             new ReanimatedPackage(),
             new SnackbarPackage(),
--- app/screens/NotificationScreen/notificationScreen.js
+++ app/screens/NotificationScreen/notificationScreen.js
@@ -1,25 +1,27 @@
 import React, { Component } from "react";
-import { Text, View, TouchableHighlight, FlatList, ScrollView } from "react-native";
+import { Text, View, TouchableOpacity, FlatList, ScrollView } from "react-native";
 import Header from "../../components/HeaderNavigationBar/HeaderNavigationBar";
 import NotificationBanner from '../../components/NotificationBanner/notificationBanner';
 import styles from './style';
 import Swipeout from 'react-native-swipeout';
 import Ionicons from "react-native-vector-icons/FontAwesome";
+import RNBottomActionSheet from 'react-native-bottom-action-sheet';
 export default class NotificationScreen extends Component {
         constructor(props) {
                 super(props);
                 this.state = {
                         data: [
-                                { id: 1, image: "https://bootdey.com/img/Content/avatar/avatar1.png", name: "Frank Odalthh", text: "Lorem ipsum dolor sit amet, ", posted: "2 hours Ago", read:1 },
-                                { id: 2, image: "https://bootdey.com/img/Content/avatar/avatar6.png", name: "John DoeLink", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.", posted: "2 hours Ago",read:0 },
-                                { id: 3, image: "https://bootdey.com/img/Content/avatar/avatar7.png", name: "March SoulLaComa", text: "Lorem ipsum dolor sit amet, consectetget dolor.", posted: "2 hours Ago",read:1 },
-                                { id: 4, image: "https://bootdey.com/img/Content/avatar/avatar2.png", name: "Finn DoRemiFaso", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing eli eget dolr.", posted: "2 hours Ago",read:0 },
-                                { id: 5, image: "https://bootdey.com/img/Content/avatar/avatar3.png", name: "Maria More More", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.", posted: "2 hours Ago",read:1 },
-                                { id: 6, image: "https://bootdey.com/img/Content/avatar/avatar4.png", name: "Clark June Boom!", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.", posted: "2 hours Ago",read:1 },
-                                { id: 7, image: "https://bootdey.com/img/Content/avatar/avatar5.png", name: "The googler", text: "Lorem ipsum dolor sit amet, et dolor.", posted: "2 hours Ago",read:0 },
+                                { id: 1, image: "https://bootdey.com/img/Content/avatar/avatar1.png", name: "Frank Odalthh", text: "Lorem ipsum dolor sit amet, ", posted: "2 hours Ago", read: 1 },
+                                { id: 2, image: "https://bootdey.com/img/Content/avatar/avatar6.png", name: "John DoeLink", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.", posted: "2 hours Ago", read: 0 },
+                                { id: 3, image: "https://bootdey.com/img/Content/avatar/avatar7.png", name: "March SoulLaComa", text: "Lorem ipsum dolor sit amet, consectetget dolor.", posted: "2 hours Ago", read: 1 },
+                                { id: 4, image: "https://bootdey.com/img/Content/avatar/avatar2.png", name: "Finn DoRemiFaso", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing eli eget dolr.", posted: "2 hours Ago", read: 0 },
+                                { id: 5, image: "https://bootdey.com/img/Content/avatar/avatar3.png", name: "Maria More More", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.", posted: "2 hours Ago", read: 1 },
+                                { id: 6, image: "https://bootdey.com/img/Content/avatar/avatar4.png", name: "Clark June Boom!", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.", posted: "2 hours Ago", read: 1 },
+                                { id: 7, image: "https://bootdey.com/img/Content/avatar/avatar5.png", name: "The googler", text: "Lorem ipsum dolor sit amet, et dolor.", posted: "2 hours Ago", read: 0 },
                         ],
                         comment: '',
-                        rowIndex:null
+                        rowIndex: null,
+                        alterView:false
 
 
                 }
@@ -27,7 +29,7 @@ export default class NotificationScreen extends Component {
         onSwipeOpen(rowIndex) {
                 this.setState({
                         rowIndex: rowIndex
-                })                
+                })
         }
         onSwipeClose(rowIndex) {
                 if (rowIndex === this.state.rowIndex) {
@@ -40,6 +42,11 @@ export default class NotificationScreen extends Component {
                 return (
                         <View style={{ flex: 1, flexDirection: 'column', }}>
                                 <Header title={"Notifications"} />
+                                <TouchableOpacity onPress={() => this.setState({alterView:true})} style={{ justifyContent: 'flex-end', marginVertical: 10, flexDirection: 'row', marginRight: 10 }}>
+                                        <Text style={{ color: "#007bff" }}>Mark All As Read</Text>
+                                        <Ionicons name={"check"} size={12} color={"#007bff"} />
+                                        <Ionicons name={"check"} size={12} color={"#007bff"} />
+                                </TouchableOpacity>
                                 <View style={styles.container}>
                                         <FlatList
                                                 style={styles.scrollView}
@@ -51,7 +58,7 @@ export default class NotificationScreen extends Component {
                                                         )
                                                 }}
                                                 keyExtractor={(item, index) => index.toString()}
-                                                renderItem={({item, index}) => {
+                                                renderItem={({ item, index }) => {
                                                         const Notification = item.item;
                                                         var unRead = [
                                                                 {
@@ -76,11 +83,11 @@ export default class NotificationScreen extends Component {
                                                                         backgroundColor: '#d9534f',
                                                                         underlayColor: 'rgba(0, 0, 0, 1, 0.6)',
                                                                         onPress: () => { alert(item.name) }
-                                                                } 
+                                                                }
                                                         ];
 
-                                                        if(item.read == 1){
-                                                                return (                                                                
+                                                        if (item.read == 1) {
+                                                                return (
                                                                         <Swipeout
                                                                                 rowIndex={index}
                                                                                 sectionId={0}
@@ -89,8 +96,8 @@ export default class NotificationScreen extends Component {
                                                                                 onClose={() => this.onSwipeClose(index)}
                                                                                 sensitivity={1000}
                                                                                 autoClose={true}
-                                                                                style={{ width: '100%', backgroundColor: 'transparent'}}                                                                        
-                                                                                right={read}>                                                                       
+                                                                                style={{ width: '100%', backgroundColor: 'transparent' }}
+                                                                                right={read}>
                                                                                 <NotificationBanner
                                                                                         read={item.read}
                                                                                         image={{ uri: item.image }}
@@ -99,9 +106,9 @@ export default class NotificationScreen extends Component {
                                                                                         text={item.text}
                                                                                 />
                                                                         </Swipeout>
-                                                                );    
-                                                        }else{
-                                                                return (                                                                
+                                                                );
+                                                        } else {
+                                                                return (
                                                                         <Swipeout
                                                                                 rowIndex={index}
                                                                                 sectionId={0}
@@ -110,8 +117,8 @@ export default class NotificationScreen extends Component {
                                                                                 onClose={() => this.onSwipeClose(index)}
                                                                                 sensitivity={1000}
                                                                                 autoClose={true}
-                                                                                style={{ width: '100%', backgroundColor: 'transparent'}}                                                                        
-                                                                                right={unRead}>                                                                       
+                                                                                style={{ width: '100%', backgroundColor: 'transparent' }}
+                                                                                right={unRead}>
                                                                                 <NotificationBanner
                                                                                         read={item.read}
                                                                                         image={{ uri: item.image }}
@@ -122,9 +129,26 @@ export default class NotificationScreen extends Component {
                                                                         </Swipeout>
                                                                 );
                                                         }
-                                                        
+
                                                 }} />
                                 </View>
+                                <RNBottomActionSheet.AlertView
+                                        visible={this.state.alterView}
+                                        title={"Awesome!"}
+                                        message={"What can we improve? Your feedback is always welcome."}
+                                        positiveText={"OK"}
+                                        positiveBackgroundColor={"#eeffee"}
+                                        positiveTextColor={"#006500"}
+                                        negativeText={"Exit"}
+                                        negativeBackgroundColor={"#ffebeb"}
+                                        negativeTextColor={"#760000"}
+                                        theme={"light"}
+                                        onPositive={() => {
+                                                console.log("positive clicked");
+                                        }}
+                                        onNegative={() => {
+                                                console.log("negative clicked");
+                                        }} />
                         </View>
                 );
         }
